3ced79877170985c6b930c532a0a3debe362607f
Claudioleroi <ikambacheryl7@gmail.com>
2024-08-20 16:58:13 +0100

    up date of a  new page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
+import { SignIn, UserButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
+import { Link } from "lucide-react";
 
 export default  async function Home() {
   const {userId} = await  auth()
@@ -17,8 +18,18 @@ export default  async function Home() {
         <div className="mt-2  flex">
 
           {isAuth && ( <Button>Discutons !</Button>
-)}
+
+       
+)}  
         </div> 
+        <p className="text-slate-200 mt-2">Customise ton ai et obtient ton assistant personnel</p>
+         <div className="flex w-full mt-4">
+            {isAuth ?( <h1> upload</h1> ):
+            (
+              <Link href='/sign-in'><Button> Q&apos;est-ce qu&apos;on attends ? Connectez - vous !</Button>   
+              </Link>
+            )}
+         </div>
       </div>
     </div>
   );
